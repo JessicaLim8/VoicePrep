0392112c3e16103ccee084968da01167f57b5b8f
JessicaLim8 <jessicalim@bell.net>
2019-09-14 17:11:15 -0400

    test push

---
+++ client/src/layout/wrapper.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from "../logo.png";
+
+export default class Landing extends Component {
+  render() {
+    return(
+      <div className="Header">
+            <img src={logo} width="5%" alt="logo"/>
+      </div>
+    )
+  }
+}
