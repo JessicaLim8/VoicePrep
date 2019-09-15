e43bf25bf5f0b2dfbf3ba21e2b8ea5f6a2bb1e33
JessicaLim8 <jessicalim@bell.net>
2019-09-14 21:36:47 -0400

    added react-router dom functinoality

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./style.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing.js";
 import Recording from "./pages/Recording.js";
+import Results from "./pages/Results.js";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { current: "Landing" };
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/record" component={Recording} />
+          <Route path="/results" component={Results} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-  onClick = () => {
-    this.setState({current: "Recording"})
-  }
-
-  render() {
-    return (
-      <div className="App">
-        { this.state.current === "Landing" ?
-          (<div className="centered">
-              <Landing/>
-              <button className="button" onClick={this.onClick}>
-                <h2 style={{cursor: "pointer"}}> Start Practicing! </h2>
-              </button>
-          </div>) :
-          <Recording/> }
-      </div>
-    );
-  }
-}
+export default App;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "../style.css";
 import logo from "../logo.png";
+import {Link} from "react-router-dom"
 
 export default class Landing extends Component {
   render() {
@@ -16,6 +17,11 @@ export default class Landing extends Component {
             <img src={logo} width="25%" alt="logo"/>
           </div>
           <div style={{padding: 10}}>
+            <Link to="/record">
+              <button className="button">
+                <h2 style={{cursor: "pointer"}}> Start Practicing! </h2>
+              </button>
+            </Link>
           </div>
       </div>
     );
