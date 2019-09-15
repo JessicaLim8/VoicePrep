fc1c646258fe6bed3990b86de128df58cfe967ee
JessicaLim8 <jessicalim@bell.net>
2019-09-14 23:28:10 -0400

    added layout for wrapping

--- client/src/layout/wrapper.js
+++ client/src/layout/wrapper.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import logo from "../logo.png";
+import { Link } from "react-router-dom";
+import logo from "../Text-logo.png";
 
 export default class Landing extends Component {
   render() {
     return(
-      <div className="Header">
-            <img src={logo} width="5%" alt="logo"/>
+      <div>
+        <div className="Header">
+          <Link to={"/"}>
+            <img src={logo} width="15%" alt="logo"/>
+          </Link>
+          <Link to={"/"}>
+            <img style={{cursor: "pointer"}} src={logo} width="5%" alt="logo"/>
+          </Link>
+        </div>
+        <div className="bodypart">
+          {this.props.children}
+        </div>
       </div>
     )
   }
