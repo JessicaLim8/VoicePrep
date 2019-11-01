495e55c21eb9a92ef6e09a27e4db6367ac112a4e
JessicaLim8 <jessicalim@bell.net>
2019-11-01 11:20:02 -0400

    added general logic

--- client/src/pages/wordcounter.js
+++ client/src/pages/wordcounter.js
@@ -1,74 +1,26 @@
-export function wordCounter(speech) { const words = speech.split(' ');
-    let fillerWords = {
-        basically: 0,
-        so: 0,
-        like: 0,
-        really: 0,
-        especially:0,
-        therefore: 0,
-        okay: 0,
-        very: 0,
-        totally: 0,
-        truly: 0,
-        literally: 0,
-        actually: 0,
-        seriously: 0,
-        right: 0,
-        but: 0,
+export const wordCounter = (speech, filler, words = []) => {
+  if (filler === true) words = fillerWords;
+  words =  words ? words.reduce((arr, key) => (arr[key.toLowerCase()] = 0, arr), {}) : {};
+  speech.split(' ').forEach((word) => {
+    if (word.toLowerCase() in words) {
+      words[word.toLowerCase()]++;
     }
-    let i;
-    for (i = 0; i < words.length; i++) {
-        switch (words[i]) {
-            case 'basically':
-                fillerWords.basically++;
-                break;
-            case 'so':
-                fillerWords.so++;
-                break;
-            case 'like':
-                fillerWords.like++;
-                break;
-            case 'really':
-                fillerWords.really++;
-                break;
-            case 'especially':
-                fillerWords.especially++;
-                break;
-            case 'therefore':
-                fillerWords.therefore++;
-                break;
-            case 'okay':
-                fillerWords.okay++;
-                break;
-            case 'very':
-                fillerWords.very++;
-                break;
-            case 'totally':
-                fillerWords.totally++;
-                break;
-            case 'truly':
-                fillerWords.truly++;
-                break;
-            case 'literally':
-                fillerWords.literally++;
-                break;
-            case 'actually':
-                fillerWords.actually++;
-                break;
-            case 'seriously':
-                fillerWords.seriously++;
-                break;
-            case 'but':
-                fillerWords.but++;
-                break;
-            case 'right':
-                fillerWords.right++;
-                break;
-          default:
-            break;
-        }
-    }
-    return fillerWords;
+  });
+  return words;
 }
 
-
+  const fillerWords = [
+    'like',
+    'basically',
+    'so',
+    'really',
+    'especially',
+    'therefore',
+    'okay',
+    'very',
+    'totally',
+    'literally',
+    'actually',
+    'right',
+    'but',
+  ]
